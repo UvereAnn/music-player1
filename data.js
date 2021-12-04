8ad8f00ebc1c7117ebc9cdbbb70dc26deef60d35
annuvere <40842990+annuvere@users.noreply.github.com>
2021-12-04 15:28:22 -0800

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Song 1',
+        path: 'music/1.mp3',
+        artist: 'PH Boy',
+        cover: 'images/img5.jpg'
+    },
+    {
+        name: 'Song 2',
+        path: 'music/2.mp3',
+        artist: 'BB',
+        cover: 'images/img2.jpg'
+    },
+    {
+        name: 'Song 3',
+        path: 'music/3.mp3',
+        artist: 'Da Burna',
+        cover: 'images/img3.jpg'
+    },
+    {
+        name: 'Song 4',
+        path: 'music/4.mp3',
+        artist: 'African Giant',
+        cover: 'images/img4.jpg'
+    },
+    {
+        name: 'Song 5',
+        path: 'music/5.mp3',
+        artist: 'Burna Boy',
+        cover: 'images/img5.jpg'
+    },
+]
